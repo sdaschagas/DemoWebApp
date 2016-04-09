c6b5909c77d0c0055d6a94ee364d887d8b04db57
Sylvia das Chagas <sylviadaschagas@Sylvias-MacBook-Pro-2.local>
2016-04-09 13:58:42 -0400

    add footer and contact page

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.get('/', function(request, response) {
 app.get('/photos', function(request, response) {
   response.render('pages/photos');
 });
-
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
